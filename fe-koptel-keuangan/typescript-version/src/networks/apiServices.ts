a644122c563333ab221e3eed69b6608f6df42335
Fadlan Kamal <fuadlankamal@gmail.com>
2025-08-16 14:20:37 +0700

    Update apiServices.ts

--- fe-koptel-keuangan/typescript-version/src/networks/apiServices.ts
+++ fe-koptel-keuangan/typescript-version/src/networks/apiServices.ts
@@ -1,3 +1,3 @@
 import React from "react";
 // export const BASE_URL = process.env.REACT_APP_BASE_URL;
-export const BASE_URL: string = process.env.VITE_BASE_URL || "http://localhost:5001";
+export const BASE_URL: string = process.env.VITE_BASE_URL || "http://10.64.210.66:5000";
