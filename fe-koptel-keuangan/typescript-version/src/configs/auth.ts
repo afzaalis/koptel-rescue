cfe3cc55a621250aeb04dc3d8ff49426a5b1e603
Fadlan Kamal <fuadlankamal@gmail.com>
2025-08-16 14:20:11 +0700

    Update auth.ts

--- fe-koptel-keuangan/typescript-version/src/configs/auth.ts
+++ fe-koptel-keuangan/typescript-version/src/configs/auth.ts
@@ -7,9 +7,9 @@
 // }
 
 export default {
-  meEndpoint: 'http://localhost:5001/api/auth/me',
-  loginEndpoint: 'http://localhost:5001/api/auth/login',
-  registerEndpoint: 'http://localhost:5001/api/auth/register',
+  meEndpoint: 'http://10.64.210.66:5000/api/auth/me',
+  loginEndpoint: 'http://10.64.210.66:5000/api/auth/login',
+  registerEndpoint: 'http://10.64.210.66:5000/api/auth/register',
   storageTokenKeyName: 'accessToken',
   onTokenExpiration: 'refreshToken'
 }
